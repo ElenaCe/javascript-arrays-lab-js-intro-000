ef252901bea67ea206c444c5bd7373d94cd30dac
Elena Cellitti <elenacellitti92@gmail.com>
2019-08-08 11:42:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 var name =["Ralph"]
 var name1 =["Bob"]
+var name2=["Broom"]
 // Add your functions and code here
 //appends a kitten to the end of the kittens array
 function destructivelyAppendKitten(name) {
@@ -18,3 +19,15 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   return kittens.shift()
 }
+
+function appendKitten(name2){
+  
+}
+  describe('appendKitten(name)', function() {
+    it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      expect(appendKitten("Broom")).toEqual(["Milo", "Otis", "Garfield", "Broom"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
