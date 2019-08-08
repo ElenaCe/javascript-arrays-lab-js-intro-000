ee642e9d94ba4dbf2ab53275cc9e29f532f78be9
Elena Cellitti <elenacellitti92@gmail.com>
2019-08-08 11:28:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 //appends a kitten to the end of the kittens array
-function destructivelyAppendKitten(name) {
-  it('', function() {
+function destructivelyAppendKitten(kittens) {
 
-    destructivelyAppendKitten('Ralph')
-    expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
-  })
-})
+  return [...kittens, 'Ralph']
+}
